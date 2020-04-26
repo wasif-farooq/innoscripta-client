9a4505ec9b8a25b2032814c3b75c41af138a4e6f
Wasif <wasif@192.168.10.3>
2020-04-26 22:48:24 +0500

    commit code

---
+++ build/precache-manifest.a89554d2859784ec901c0dd855bcb801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9be504eb3eea465f21a2a11e6c577fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "206ca8e95bc180491705",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "da98bb8497c281e7c512",
+    "url": "/static/css/main.eb52045f.chunk.css"
+  },
+  {
+    "revision": "206ca8e95bc180491705",
+    "url": "/static/js/2.f09f68a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f09f68a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da98bb8497c281e7c512",
+    "url": "/static/js/main.344c4711.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
